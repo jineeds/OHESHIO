5365d779b86b7234dbdcd17d19c68bcdd008121a
seeo00 <csyoung0929@gmail.com>
2025-03-04 17:13:36 +0900

    폴더구조

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
     <>
-      <div className='bg-red-500'>test</div>
+      <div className="bg-red-500">test</div>
     </>
   );
 }
