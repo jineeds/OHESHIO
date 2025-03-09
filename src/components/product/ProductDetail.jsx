915d8c8bee5d8a38db5dea23b0e4625f32e70950
Juunyong <qkrwnsdyd212@gmail.com>
2025-03-09 18:26:51 +0900

    styled for product

--- src/components/product/ProductDetail.jsx
+++ src/components/product/ProductDetail.jsx
@@ -5,7 +5,11 @@ import Buttons from '../../ui/Buttons';
 const ProductDetail = () => {
     const doc = React.useRef(null);
     const doc2 = React.useRef(null);
-
+    const product_detail1 = React.useRef(null);
+    const product_detail2 = React.useRef(null);
+    const product_detail3 = React.useRef(null);
+    const product_detail4 = React.useRef(null);
+    
     React.useEffect(() => {
         const typed = new Typed(doc.current, {
             strings: [`Wares PANT SEAL`],
@@ -15,11 +19,31 @@ const ProductDetail = () => {
             strings: [`239.00 USD`],
             typeSpeed: 50,
         });
+        const product_typed = new Typed(product_detail1.current, {
+            strings: [`- 100% faux fur`],
+            typeSpeed: 50,
+        });
+        const product_typed2 = new Typed(product_detail2.current, {
+            strings: [`- 100% faux fur`],
+            typeSpeed: 50,
+        });
+        const product_typed3 = new Typed(product_detail3.current, {
+            strings: [`- 100% faux fur`],
+            typeSpeed: 50,
+        });
+        const product_typed4 = new Typed(product_detail4.current, {
+            strings: [`- 100% faux fur`],
+            typeSpeed: 50,
+        });
 
         return () => {
-            // Destroy Typed instance during cleanup to stop animation
+            
             typed.destroy();
             typed2.destroy();
+            product_typed.destroy();
+            product_typed2.destroy();
+            product_typed3.destroy();
+            product_typed4.destroy();
         };
     }, []);
     return (
@@ -46,30 +70,28 @@ const ProductDetail = () => {
             <div className="lower_product_info">
                 <div></div>
                 <div className="w-full flex mt-5 flex-wrap relative">
-                    <span className="w-[50px] h-[50px] rounded-full mr-[10px] text-center flex items-center justify-center uppercase relative mb-[10px]">
+                <span className="w-[50px] h-[50px] rounded-full mr-[10px] text-center flex items-center justify-center uppercase relative mb-[10px]">
                         <input className="absolute opacity-0 cursor-pointer h-full w-full top-0 left-0" type="text" />{' '}
-                        <span className="absolute top-0 left-0 h-full w-full bg-transparent rounded-full border border-black -z-10"></span>
-                        XS
+                        <span className="absolute top-0 left-0 h-full w-full bg-[#F1F5F9] rounded-full border-transprant -z-10"></span>    XS
                     </span>
                     <span className="w-[50px] h-[50px] rounded-full mr-[10px] text-center flex items-center justify-center uppercase relative mb-[10px]">
                         <input className="absolute opacity-0 cursor-pointer h-full w-full top-0 left-0" type="text" />{' '}
-                        <span className="absolute top-0 left-0 h-full w-full bg-transparent rounded-full border border-black -z-10"></span>
-                        S
-                    </span>
+                        <span className="absolute top-0 left-0 h-full w-full bg-[#F1F5F9] rounded-full border-transprant -z-10"></span>    S
+                    </span> 
                     <span className="w-[50px] h-[50px] rounded-full mr-[10px] text-center flex items-center justify-center uppercase relative mb-[10px]">
                         <input className="absolute opacity-0 cursor-pointer h-full w-full top-0 left-0" type="text" />{' '}
-                        <span className="absolute top-0 left-0 h-full w-full bg-transparent rounded-full border border-black -z-10"></span>
+                        <span className="absolute top-0 left-0 h-full w-full bg-[#F1F5F9] rounded-full border-transprant -z-10"></span>
+                        
                         M
                     </span>
                     <span className="w-[50px] h-[50px] rounded-full mr-[10px] text-center flex items-center justify-center uppercase relative mb-[10px]">
                         <input className="absolute opacity-0 cursor-pointer h-full w-full top-0 left-0" type="text" />{' '}
-                        <span className="absolute top-0 left-0 h-full w-full bg-transparent rounded-full border border-black -z-10"></span>
+                        <span className="absolute top-0 left-0 h-full w-full bg-[#F1F5F9] rounded-full border-transprant -z-10"></span>
                         L
                     </span>
                     <span className="w-[50px] h-[50px] rounded-full mr-[10px] text-center flex items-center justify-center uppercase relative mb-[10px]">
                         <input className="absolute opacity-0 cursor-pointer h-full w-full top-0 left-0" type="text" />{' '}
-                        <span className="absolute top-0 left-0 h-full w-full bg-transparent rounded-full border border-black -z-10"></span>
-                        XL
+                        <span className="absolute top-0 left-0 h-full w-full bg-[#F1F5F9] rounded-full border-transprant -z-10"></span>    XL
                     </span>
                 </div>
                 <div>
@@ -87,18 +109,18 @@ const ProductDetail = () => {
 
                 <div className="product_details_toggle">
                     <span className="details_toggle_description">product details</span>
-                    <div className="product_details_document hidden">
-                        <div>- 100% faux fur</div>
-                        <div>- 100% faux fur</div>
-                        <div>- 100% faux fur</div>
-                        <div>- 100% faux fur</div>
+                    <div className="product_details_document">
+                        <p ref={product_detail1}></p>
+                        <p ref={product_detail2}></p>
+                        <p ref={product_detail3}></p>
+                        <p ref={product_detail4}></p>
                     </div>
                 </div>
                 <div className="sizing_chart_toggle">
                     <span className="sizing_toggle_description">sizing chart</span>
                 </div>
                 <div>
-                    <span>styled with</span>
+                    <span>styled with ↓</span>
                 </div>
             </div>
         </div>
